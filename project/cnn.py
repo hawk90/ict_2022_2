6cb3e95df572a1dc47cef855429dec028a170448
Ryker-DS <68882630+Ryker-DS@users.noreply.github.com>
2022-08-16 19:05:52 +0900

    Create cnn.py

---
+++ project/cnn.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import load_sample_image
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+china = load_sample_image("china.jpg") / 255
+flower = load_sample_image("flower.jpg") / 255
+images = np.array([china, flower])
+batch_size, height, width, channels = images.shape
+
+filters = np.zeros(shape=(7,7,channels, 2), dtype=np.float32)
+filters[:, 3:, :, 0] = 1
+filters[3, :, :, 1] = 1
+
+outputs = tf.nn.conv2d(images, filters, strides=1, padding="SAME")
+plt.imshow(outputs[0,:,:,1], cmap="gray")
+plt.show()
