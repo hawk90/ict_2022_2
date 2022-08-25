ad32597555389bf02fa0b44c15c82fc2f12e32ff
hawk90 <hawking90a@gmail.com>
2022-08-25 18:50:18 +0900

    Update model.py

---
+++ Lecture_code/20days/EfficientDet/model.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import (BatchNormalization, Conv2D, Dense,
+                                     Dropout, GlobalAveragePooling2D, Layer)
+
+
+class MBCov1(Layer):
+    def __init__(self, **kwargs):
+        pass
+
+    def call(self, inputs):
+        pass
+
+
+class MBCov6(Layer):
+    def __init__(self, **kwargs):
+        pass
+
+    def call(self, inputs):
+        pass
+
+
+class Efficient(Model):
+    def __init__(self, output_dim, **kwargs):
+        pass
+
+    def call(self, inputs):
+        pass
