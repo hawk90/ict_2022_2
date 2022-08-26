27c318d053d8c1f4ca04e7b13280693cd74327a3
hawk90 <hawking90a@gmail.com>
2022-08-26 19:04:39 +0900

    Update model.py

--- Lecture_code/20days/EfficientDet/model.py
+++ Lecture_code/20days/EfficientDet/model.py
@@ -1,22 +1,45 @@
 import tensorflow as tf
+from tensorflow.kears.activats import relu
 from tensorflow.keras import Model
 from tensorflow.keras.layers import (BatchNormalization, Conv2D, Dense,
                                      DepthwiseConv2D, Dropout,
-                                     GlobalAveragePooling2D, Layer)
+                                     GlobalAveragePooling2D, Layer, Reshape)
 
-class SE(Layer):
+
+class Swish(Layer):
     def __init__(self, **kwargs):
         pass
 
-    def call(self, inputs):
+    def call(self, x):
+        return x * relu(x + 3) / 6
+
+
+class SE(Layer):
+    def __init__(self, filters, **kwargs):
+        self.filters = filters
         pass
+
+    def call(self, inputs):
+        x = GlobalAveragePooling2D()(inputs)
+        x = Reshape(1, 1, self.filters)(x)
+        x = Conv2D((1, 1, self.filters), strides=1, activation="relu", padding="same")(
+            x
+        )
+        x = Conv2D(self.filters, strides=1, activation="sigmoid", paddig="same")(x)
+        pass
+
 
 class MBCov1(Layer):
     def __init__(self, **kwargs):
         pass
 
     def call(self, inputs):
-        pass
+        x = DepthwiseConv2D()(input)
+        x = BatchNormalization()(x)
+        x = Swish()(x)
+        x = SE()(x)
+        x = Conv2D()(x)
+        return BatchNormalization()(x)
 
 
 class MBCov6(Layer):
@@ -24,7 +47,15 @@ class MBCov6(Layer):
         pass
 
     def call(self, inputs):
-        pass
+        x = Conv2D()(inputs)
+        x = Swish()(x)
+        x = BatchNormalization()(x)
+        x = DepthwiseConv2D()(x)
+        x = BatchNormalization()(x)
+        x = Swish()(x)
+        x = SE()(x)
+        x = Conv2D()(x)
+        return BatchNormalization()(x)
 
 
 class Efficient(Model):
